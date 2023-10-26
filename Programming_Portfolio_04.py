1ee68be49d448a0ac88bd30055dc5018cefa22c8
AdamRandal <A.Randal2254@student.leedsbeckett.ac.uk>
2023-10-26 10:06:53 +0100

    Programming Portfolio 4

--- Programming_Portfolio_04.py
+++ Programming_Portfolio_04.py
@@ -1,1 +1,25 @@
-#Hello
+#One
+#Userinput = int(input("Enter a num: "))
+def RangeCheck(Userinput):
+    if Userinput <= 100 and Userinput >= 0:
+        print("In Range")
+    else:
+        print("Not In Range")
+
+#RangeCheck(Userinput)
+
+#Two
+#UserInput = input("Enter a string: ")
+def CaseCheck(UserInput):
+    U = 0
+    L = 0
+    for i in UserInput:
+        if i.isupper():
+            U += 1
+        elif i.islower():
+            L += 1
+    print("Uppercase =",U,"\nLowercase =",L)
+
+#CaseCheck(UserInput)
+
+#Three
